58b8b7dd9b724a0119ee1c0775fafa7a82e7a42d
arorakirti2212 <arorakirti2212@gmail.com>
2023-06-26 00:17:23 +0530

    view details of particular blog added - redirect to post-details

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,15 @@
 
 
 import { useState } from "react";
+import { useNavigate } from "react-router";
 import { Firestore } from "./firebase";
 
 export function Home(){
     
     var [listItem, setListItem] = useState([])
    
+
+    
     Firestore.collection('Posts').onSnapshot((snapshot) => {
               
         const list = snapshot.docs.map((doc) => {
@@ -22,13 +25,24 @@ export function Home(){
         // console.log(list);
          
     });
+
+    const navigate = useNavigate();
+    const handleViewDetails = (id) => {
+        navigate(`./post-details/${id}`);
+    }
+
     return(
         <div> 
         {
             //  ---- CSS PENDING --- 
             listItem.map((l)=>{
                 // Blog post card with title, subtitle and content.
-                return (<div key={l.key}><h1>{l.title}</h1><h3>{l.subtitle}</h3><p>{l.content}</p></div>)
+                return (
+                    <div key={l.key}>
+                        <h1>{l.title}</h1>
+                        <h4>{l.subtitle}</h4>
+                        <button onClick={() => handleViewDetails(l.key)}>View Details</button>
+                    </div>)
             })
 
 
--- src/postdetails.js
+++ src/postdetails.js
@@ -1,7 +1,69 @@
+import { useState, useRef } from "react";
+import { Firestore } from "./firebase";
+
+
 export function PostDetails(){
 
+    var [listItem, setListItem] = useState([])
+
+    console.log('1. ---- ')
+    const pathname = window.location.pathname.split('/');
+
+    console.log('2. ---- ')
+    const postId = pathname[pathname.length-1];
+
+    console.log('3. ---- ')
+    const db = Firestore.collection('Posts');
+
+
+    console.log('4. ---- ')
+    
+    var token = useRef('false');
+    const getDbData = () => {db.onSnapshot(snapshot => {
+
+        console.log('5. ---- ')
+        var list = []
+        
+        snapshot.docs.map(doc => {
+
+
+            console.log('6. ---- ')
+
+            
+            var data = doc.data();
+            // console.log(doc.id, data.title);
+
+            if(doc.id === postId){
+                data['key'] = doc.id;
+                list.push(data);                
+            }
+            return '';
+        })
+
+        console.log('7. ---- ')
+        setListItem(list);
+        token.current = 'true';
+        // console.log(listItem);
+    })
+    }
+    if(token.current === 'false'){
+        console.log(getDbData())
+    }
+    console.log(token);
+
+
+
+
+console.log('9. ---- ')
+
+
     return(
-        <div> Inside Post Detail Page!!</div>
+        
+        <div> {
+            listItem.map(element => {
+                return <h1 style={{color:'pink'}} key={element.key}>{element.title}</h1>
+            })
+        }</div>
     );
 }
 
