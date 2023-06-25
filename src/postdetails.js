7ca884c46d7df7de9bd1a25d6adcde9834eb589a
arorakirti2212 <arorakirti2212@gmail.com>
2023-06-26 00:21:00 +0530

    post-details -- on click of view detail of corresponding blog

--- src/postdetails.js
+++ src/postdetails.js
@@ -6,29 +6,20 @@ export function PostDetails(){
 
     var [listItem, setListItem] = useState([])
 
-    console.log('1. ---- ')
     const pathname = window.location.pathname.split('/');
 
-    console.log('2. ---- ')
     const postId = pathname[pathname.length-1];
 
-    console.log('3. ---- ')
     const db = Firestore.collection('Posts');
 
 
-    console.log('4. ---- ')
     
     var token = useRef('false');
     const getDbData = () => {db.onSnapshot(snapshot => {
 
-        console.log('5. ---- ')
         var list = []
         
         snapshot.docs.map(doc => {
-
-
-            console.log('6. ---- ')
-
             
             var data = doc.data();
             // console.log(doc.id, data.title);
@@ -40,10 +31,10 @@ export function PostDetails(){
             return '';
         })
 
-        console.log('7. ---- ')
         setListItem(list);
+
+        //useRef token for it to only fetch data once rather than get involved in an infinite loop.
         token.current = 'true';
-        // console.log(listItem);
     })
     }
     if(token.current === 'false'){
@@ -51,10 +42,6 @@ export function PostDetails(){
     }
     console.log(token);
 
-
-
-
-console.log('9. ---- ')
 
 
     return(
